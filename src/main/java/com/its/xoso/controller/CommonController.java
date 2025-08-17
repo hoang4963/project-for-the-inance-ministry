86031c1f1a51de027d962f5f8fa8140c3288579d
hoang4963 <hoangth.finbox@gmail.com>
2025-08-17 18:00:07 +0700

    update

--- src/main/java/com/its/xoso/controller/CommonController.java
+++ src/main/java/com/its/xoso/controller/CommonController.java
@@ -1,6 +1,11 @@
 package com.its.xoso.controller;
 
 
+import com.its.xoso.entity.DanhMuc;
+import com.its.xoso.entity.LinhVuc;
+import com.its.xoso.entity.LoaiHinh;
+import com.its.xoso.entity.TieuChi;
+import com.its.xoso.entity.Tinh;
 import com.its.xoso.repository.DanhMucRepository;
 import com.its.xoso.repository.LinhVucRepository;
 import com.its.xoso.repository.LoaiHinhRepository;
@@ -14,6 +19,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -83,5 +89,31 @@ public class CommonController {
     @GetMapping("/get-all-loaiHinh")
     public ResponseEntity<List<?>> getLoaiHinh() {
         return ResponseEntity.ok(loaiHinhRepository.findAll());
+    }
+
+
+    @GetMapping("/get-tinh/{id}")
+    public ResponseEntity<Tinh> getTinhById(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(tinhRepository.findById(id).orElse(null));
+    }
+
+    @GetMapping("/get-linhVuc/{id}")
+    public ResponseEntity<LinhVuc> getLinhVucById(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(linhVucRepository.findById(id).orElse(null));
+    }
+
+    @GetMapping("/get-tieuChi/{id}")
+    public ResponseEntity<TieuChi> getTieuChiById(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(tieuChiRepository.findById(id).orElse(null));
+    }
+
+    @GetMapping("/get-danhMuc/{id}")
+    public ResponseEntity<DanhMuc> getDanhMucById(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(danhMucRepository.findById(id).orElse(null));
+    }
+
+    @GetMapping("/get-loaiHinh/{id}")
+    public ResponseEntity<LoaiHinh> getLoaiHinhById(@PathVariable("id") Integer id) {
+        return ResponseEntity.ok(loaiHinhRepository.findById(id).orElse(null));
     }
 }
--- src/main/java/com/its/xoso/repository/TinhRepository.java
+++ src/main/java/com/its/xoso/repository/TinhRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface TinhRepository extends JpaRepository<Tinh, String> {
+public interface TinhRepository extends JpaRepository<Tinh, Integer> {
     Tinh findByTenTinh(String tenTinh);
 
 }
